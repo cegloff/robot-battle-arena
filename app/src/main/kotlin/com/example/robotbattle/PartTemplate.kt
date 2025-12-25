ad52b024fc5799ab377ab2b4aa83175190e799cb
cegloff <chris@egloff.tech>
2025-12-25 10:33:54 -0700

    Create app/src/main/kotlin/com/example/robotbattle/PartTemplate.kt

---
+++ app/src/main/kotlin/com/example/robotbattle/PartTemplate.kt
@@ -0,0 +1,14 @@
+package com.example.robotbattle
+
+data class PartTemplate(
+    val type: String,
+    val name: String,
+    val health: Int,
+    val damage: Int? = null,
+    val speed: Int? = null,
+    val range: Int? = null
+) {
+    fun toPart(): RobotPart {
+        return RobotPart(name, health, damage ?: 0, speed ?: 0, range ?: 0)
+    }
+}
